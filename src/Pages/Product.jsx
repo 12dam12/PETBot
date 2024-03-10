47459fa0a1acf7adb1eaff1dfdabc99df11eef23
vnd <vodam131021@gmail.com>
2024-03-11 04:11:56 +0700

    sửa đổi lần 1

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -16,21 +16,21 @@ function Product({ handleAdd }) {
       <div class="container">
         <h1 className="product_h1 mt-5">Selling Products</h1>
         <div class="row mt-5">
-          {records.map((itemm) => (
+          {records.map((item) => (
             <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
               <div className="card product-item border-0 mb-4">
                 <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0 h-div-image">
-                  <Link to={`detail/${itemm.id}`}>
-                    <img className="h-image img-fluid" src={itemm.image} />
+                  <Link to={`/detail/${item.id}`}>
+                    <img className="h-image img-fluid" src={item.image} />
                   </Link>
                 </div>
                 <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                  <h6 className="text-truncate mb-3">{itemm.name}</h6>
+                  <h6 className="text-truncate mb-3">{item.name}</h6>
                   <div className="d-flex justify-content-center">
-                    <h6>${itemm.new_price}</h6>
+                    <h6>${item.new_price}</h6>
                     {"     -"}
                     <h6 className="text-muted ml-2">
-                      <del>${itemm.old_price}</del>
+                      <del>${item.old_price}</del>
                     </h6>
                   </div>
                 </div>
@@ -38,7 +38,7 @@ function Product({ handleAdd }) {
                 <div className="card-footer d-flex justify-content-between bg-light border">
                   <button className="btn btn-outline-secondary">
                     <Link
-                      to={`/detail/${itemm.id}`}
+                      to={`/detail/${item.id}`}
                       className="btn btn-sm text-dark p-0"
                     >
                       <i className="fas fa-eye text-primary mr-1"></i> View
@@ -49,7 +49,7 @@ function Product({ handleAdd }) {
                   <button
                     className="btn btn-outline-secondary p-0"
                     type="button"
-                    onClick={() => handleAdd(itemm)}
+                    onClick={() => handleAdd(item)}
                   >
                     <i className="fas fa-shopping-cart text-primary mr-1"></i>{" "}
                     Add To Cart
